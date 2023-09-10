22f99e066d19068bf14f7056185dba73b3c82d72
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-09-10 15:00:56 -0400

    added cards for excercises

--- src/ExercisesIndex.jsx
+++ src/ExercisesIndex.jsx
@@ -11,15 +11,34 @@ export function ExercisesIndex(props) {
   };
 
   return (
-    <div>
-      <h1>All Exercises</h1>
-      {props.exercises.map((exercise) => (
-        <div key={exercise.id}>
-          <h2>{exercise.name}</h2>
-          <button onClick={() => props.onShowExercise(exercise)}>More info</button>
-        </div>
-      ))}
-      <ExercisesNew onCreateExercise={handleCreateExercise} />
-    </div>
+    <>
+      <br />
+      <div className="card text-center">
+        {props.exercises.map((exercise) => (
+          <div key={exercise.id}>
+            <div className="card-header">{exercise.name}</div>
+            <div className="card-body">
+              <button onClick={() => props.onShowExercise(exercise)}>More info</button>
+              <h5 className="card-title">{exercise.description}</h5>
+            </div>
+          </div>
+        ))}
+        <ExercisesNew onCreateExercise={handleCreateExercise} />
+      </div>
+    </>
   );
+}
+
+{
+  /* <div>
+  <h1>All Exercises</h1>
+  {props.exercises.map((exercise) => (
+    <div key={exercise.id}>
+      <h2>{exercise.name}</h2>
+      <button onClick={() => props.onShowExercise(exercise)}>More info</button>
+    </div>
+  ))}
+  <br />
+  <ExercisesNew onCreateExercise={handleCreateExercise} />
+</div>; */
 }
